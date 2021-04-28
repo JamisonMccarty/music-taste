5280fc6b5d151b8bbd201d723b8a88aec966c903
Jamison McCarty <jmcc13@vt.edu>
2021-04-27 21:46:15 -0400

    updated configs to work on api and firebase issues

--- src/components/Firebase/fbConfig.js
+++ src/components/Firebase/fbConfig.js
@@ -10,15 +10,25 @@ import 'firebase/auth';
 // messagingSenderId: "916980291168",
 // appId: "1:916980291168:web:2c2e0eb6a5a2f13baa9505"
 
-const config = {
-    apiKey: "AIzaSyDV5Yu4-tZwNHoR37Q1ItsDATchpYd5nG0",
-    authDomain: "music-taste-app.firebaseapp.com",
-    databaseURL: "https://music-taste-app.firebaseio.com",
-    projectId: "music-taste-app",
-    storageBucket: "music-taste-app.appspot.com",
-    messagingSenderId: "916980291168",
-    appId: "1:916980291168:web:2c2e0eb6a5a2f13baa9505"
-};
+// const config = {
+//     apiKey: "AIzaSyDV5Yu4-tZwNHoR37Q1ItsDATchpYd5nG0",
+//     authDomain: "music-taste-app.firebaseapp.com",
+//     databaseURL: "https://music-taste-app.firebaseio.com",
+//     projectId: "music-taste-app",
+//     storageBucket: "music-taste-app.appspot.com",
+//     messagingSenderId: "916980291168",
+//     appId: "1:916980291168:web:2c2e0eb6a5a2f13baa9505"
+// };
+
+export const config = {
+    apiKey: "AIzaSyDwkJAEZGZoZsd2v-E1hJid_fca3S-CWB8",
+    authDomain: "music-taste-app-38773.firebaseapp.com",
+    databaseURL: "https://music-taste-app-38773.firebaseio.com",
+    projectId: "music-taste-app-38773",
+    storageBucket: "",
+    messagingSenderId: "798086738297",
+    appId: "1:798086738297:web:a6c0a42235594b54"
+  };
 
 firebase.initializeApp(config);
 firebase.firestore().settings({timestampsInSnapshots: true});
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,14 +1,24 @@
 // Taste Dive API key
 // const tasteDiveAPI = '345217-TasteEve-Q79CTN8V';
 
-export const firebaseConfig = {
-    apiKey: "AIzaSyBSx7G9A8ldaNogd9D62MnCqs3aqHFRlEY",
-    authDomain: "ga-jsr-709.firebaseapp.com",
-    databaseURL: "https://ga-jsr-709.firebaseio.com",
-    projectId: "ga-jsr-709",
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyBSx7G9A8ldaNogd9D62MnCqs3aqHFRlEY",
+//     authDomain: "ga-jsr-709.firebaseapp.com",
+//     databaseURL: "https://ga-jsr-709.firebaseio.com",
+//     projectId: "ga-jsr-709",
+//     storageBucket: "",
+//     messagingSenderId: "788848286036",
+//     appId: "1:788848286036:web:a6c0a42235594b54"
+//   };
+
+  export const firebaseConfig = {
+    apiKey: "AIzaSyDwkJAEZGZoZsd2v-E1hJid_fca3S-CWB8",
+    authDomain: "music-taste-app-38773.firebaseapp.com",
+    databaseURL: "https://music-taste-app-38773.firebaseio.com",
+    projectId: "music-taste-app-38773",
     storageBucket: "",
-    messagingSenderId: "788848286036",
-    appId: "1:788848286036:web:a6c0a42235594b54"
+    messagingSenderId: "798086738297",
+    appId: "1:798086738297:web:a6c0a42235594b54"
   };
 
 //Eventbrite API information
